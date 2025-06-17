421fe81a8e930d3babe973597745d546d3b26a16
Basavaraja Hiruru <basavaraj.hirur746@gmail.com>
2025-06-17 13:42:40 +0530

    changed port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 7777;
 const connectDB = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
